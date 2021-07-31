bc8b66e1a37e6abb5805780d6148267fdf72bf00
mohmmadnoorjebreen <mohammadnoormjk1998@gmail.com>
2021-07-31 11:38:05 +0300

    save the update

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,73 +1,73 @@
-from abc import ABC, abstractmethod, abstractstaticmethod, abstractclassmethod
 
 
-class Band(ABC):
-    created_Band = 0
+class Band():
+    instances = []
 
-    def __init__(self, name , members : list):
+    def __init__(self, name , members ):
        self.name = name
        self.members = members
+       Band.instances.append(self.name)
      
     def play_solos(self):
-        return['face melting guitar solo',"bom bom buh bom","rattle boom crash"]    
-    def __repr__(name):
-        return 'Guitarist instance. Name = {}'.format(name)   
-    def __str__ (name):
-        return 'My name is {} and I play guitar'.format(name)   
+       solo_list = []
+       for member in self.members:
+          solo_list.append(member.play_solo())
+       return solo_list 
+       
+    def __str__ (self):
+         return '{}'.format(self.name)  
+    
+    def __repr__(self):
+         return '{}'.format(self.name)  
 
+     
     @classmethod
-    def to_list (cls):
-        if Band.created_Band == 0:
-           Band.created_Band =+1
-           return []
-        else:
-            return[Band.created_Band]
+    def to_list(cls):
+       return cls.instances
       
 
+class Musician():
+   
+    def __init__(self,name):
+            self.name= name
 
-class Musician(Band):
-    
     def __str__ (self):
-           return 'My name is {} and I play bass'.format(self.name)    
+           return 'My name is {} and I play {}'.format(self.name,self.get_instrument())    
     def __repr__(self):
-           return 'Bassist instance. Name = {}'.format(self.name)  
-    def get_instrument():
-        pass
-    def play_solos():
-        pass   
+           return '{} instance. Name = {}'.format(self.__class__.__name__,self.name)  
+
+    def get_instrument(self):
+      return '{}'.format(self.name)
     
+    def play_solo(self):
+      return '{}'.format(self.name)
+    
+
 class Guitarist(Musician):
-        def __init__(self, name):
-           self.name = name
-        def __str__ (self):
-           return 'My name is {} and I play guitar'.format(self.name)    
-        def __repr__(self):
-           return 'Guitarist instance. Name = {}'.format(self.name)
-        def get_instrument(self):
-           return "guitar"
-        def play_solo(self):
-            return  'face melting guitar solo'  
+
+   def get_instrument(self):
+      return "guitar"
+   def play_solo(self):
+      return  'face melting guitar solo'  
         
 class Drummer(Musician):
-        def __init__(self, name):
-           self.name = name
-        def __str__ (self):
-           return 'My name is {} and I play drums'.format(self.name)    
-        def __repr__(self):
-           return 'Drummer instance. Name = {}'.format(self.name) 
-        def get_instrument(self):
-           return  "drums" 
-        def play_solo(self):
-           return  "rattle boom crash"         
+
+   def get_instrument(self):
+      return  "drums" 
+   def play_solo(self):
+      return  "rattle boom crash"         
        
 class Bassist(Musician):
-        def __init__(self, name):
-           self.name = name   
-        def get_instrument(self):
-           return  "bass"
-        def play_solo(self):
-           return "bom bom buh bom"
+   def get_instrument(self):
+      return  "bass"
+   def play_solo(self):
+      return "bom bom buh bom"
 
 
 
 
+if __name__ == '__main__':
+   print('work')
+   Band('x',[1,2,3])
+   print(Band.play_solos())
+   
